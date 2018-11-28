c91224d5eb1929afb55ff369bb034d78bad5fbc2
Érick <erickcontato@hotmail.com>
2018-11-28 15:46:01 -0200

    On branch img_uploader  Changes to be committed: 	modified:   app/ui5/webapp/controller/ImageClassification.controller.js

--- app/ui5/webapp/controller/ImageClassification.controller.js
+++ app/ui5/webapp/controller/ImageClassification.controller.js
@@ -8,5 +8,22 @@ sap.ui.define([
 	"use strict";
 	return Controller.extend("sap.mlf.timeverde.controller.ImageClassification", {
 	
+		onInit: function () {
+			sap.ui.getCore().attachValidationError(function (evt) {
+				var control = evt.getParameter("element");
+				if (control && control.setValueState) {
+					control.setValueState("Error");
+				}
+			});
+
+			sap.ui.getCore().attachValidationSuccess(function (evt) {
+				var control = evt.getParameter("element");
+				if (control && control.setValueState) {
+					control.setValueState("None");
+				}
+			});
+
+			this.getView().byId("fileUploader").addStyleClass("fileUploaderStyle1");
+		}
 	});
 });
